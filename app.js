af87b478d13d462bef1d5bd3108e815d68e3b6f0
VYSYAKH A <43199411+vkajith@users.noreply.github.com>
2021-04-11 09:47:09 +0530

    minor

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Select DOM
+//DOM
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
